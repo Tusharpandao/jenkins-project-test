8a74a2c78efe7b50d59d2cdee9b7c80efd7f8bf6
Tushar Pandao <pandaotushar1@gmail.com>
2024-02-25 16:18:34 +0530

    frist

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("This is first project using ci/cd tools");
+        System.out.println(new Date());
+    }
+}
